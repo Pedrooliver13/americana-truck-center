4adea61cf3fbe958c1cc48e4d441455d320d3eb1
pedrooliver13 <pedro.oliveira@qyon.com>
2024-11-05 19:23:13 -0300

    fix: fix get all clients

--- src/hooks/clients/useGetAllClients.tsx
+++ src/hooks/clients/useGetAllClients.tsx
@@ -11,13 +11,20 @@ import { getAllClients } from 'services/clients/getAllClients';
 
 export const useGetAllClients = (id?: string) => {
   const location = useLocation();
-  const URLToEnableGet = ['/', '/clients', '/tasks/new', '/prices/new'];
+  const URLToEnableGet = [
+    '/',
+    '/clients',
+    '/tasks/new',
+    `/tasks/${id}`,
+    '/prices/new',
+    `/prices/${id}`,
+  ];
 
   const response = useQuery<Array<Clients>>({
     queryKey: ['clients'],
     queryFn: () => getAllClients() as Promise<Array<Clients>>,
     staleTime: 1000 * 60 * 30, // 30 minutes
-    enabled: Boolean(!id) && URLToEnableGet.includes(location.pathname),
+    enabled: Boolean(!id) || URLToEnableGet.includes(location.pathname),
   });
 
   if (!response?.data || !Array.isArray(response?.data)) {
