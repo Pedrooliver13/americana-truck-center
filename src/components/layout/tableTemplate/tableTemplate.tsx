63591697e576386e1435e882dac56e3e43eafad8
pedrooliver13 <pedro_oliver13@hotmail.com>
2025-02-28 12:54:52 -0300

    feat: change default itens per page

--- src/components/layout/tableTemplate/tableTemplate.tsx
+++ src/components/layout/tableTemplate/tableTemplate.tsx
@@ -172,7 +172,7 @@ export const TableTemplate = (props: TableTemplateProps): ReactElement => {
             bordered
             pagination={{
               showSizeChanger: true,
-              defaultPageSize: 5,
+              defaultPageSize: 10,
               pageSizeOptions: ['5', '10', '20', '30', '50', '100'],
             }}
           />
